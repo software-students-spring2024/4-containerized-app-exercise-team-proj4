0f108b1d3e7b51ff3ca597b8aa3a14f7dda56d82
ZijieZha0 <zz3589@nyu.edu>
2024-04-07 13:43:53 -0400

    setup for front&backend

---
+++ web-app/app.py
@@ -0,0 +1,19 @@
+"""
+Simple Flask application to serve the front end of the Speech to Text project.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    Render the main page of the web application.
+    """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
