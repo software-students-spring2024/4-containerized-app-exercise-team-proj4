da45a66207ebed7c333e3389ff94f5e607d04aae
WayyGood <natedan44@gmail.com>
2024-04-09 22:20:20 -0400

    attempt to fix pylint error

--- web-app/tests.py
+++ web-app/tests.py
@@ -3,15 +3,18 @@ from app import app
 
 
 @pytest.fixture
-def client():
+def test_client():
     with app.test_client() as client:
         yield client
 
 
-def test_index(client):
-    response = client.get("/")
+def test_index(test_client):
+    """
+    Test index route.
+    """
+    response = test_client.get("/")
     assert response.status_code == 200
-    assert b"Hello, World!" in response.data 
+    assert b"Hello, World!" in response.data  
 
 
 if __name__ == "__main__":
